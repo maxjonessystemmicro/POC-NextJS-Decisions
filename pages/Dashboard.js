ba1727d459cd5d5adebb9ed6cb77c71dc61dfb5a
maxjonessystemmicro <max.jones@systemmicro.ca>
2024-09-09 11:21:25 -0400

    Data Container Creation

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -3,11 +3,30 @@ import styles from './dashboard.module.css';
 
 
 function Dashboard() {
-    const [score, setScore] = useState({test:'flow'});
+    const [ExpectedPayload, setExpectedPayload] = useState(
+        {UserEmail:null,Message:'Empty',LastUpdate:null,LastUpdatedBy:null,ReturnSession: null}
+
+    );
+
+    function Clear() {
+        setExpectedPayload({UserEmail:null,Message:'Empty',LastUpdate:null,LastUpdatedBy:null,ReturnSession: null});
+    }
 
     return (
         <div>
-         <div className={styles.dataContainer}>{score.test}</div>
+            <div className={styles.dataContainer}>
+                <div>UserEmail: {ExpectedPayload.UserEmail}</div>
+                <div>Message: {ExpectedPayload.Message}</div>
+                <div>LastUpdate: {ExpectedPayload.LastUpdate}</div>
+                <div>LastUpdateBy: {ExpectedPayload.LastUpdatedBy}</div>
+                <div><br/></div>
+                <div>Return Session: {ExpectedPayload.ReturnSession}</div>
+            </div>
+            <div className={styles.buttonDiv}>
+                <button className={styles.button}>Return</button>
+                <button className={styles.button}>Reset</button>
+                <button className={styles.button} onClick={() => Clear(1)}>Clear</button>
+            </div>
         </div>
     );
 }
